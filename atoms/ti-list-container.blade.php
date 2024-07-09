260a3c94538c1be1dd356a710b4652cc170015ed
David Maye <dmconsultorweb@gmail.com>
2024-07-09 09:15:20 +0200

    feat: add vertical list item

---
+++ atoms/ti-list-container.blade.php
@@ -0,0 +1,5 @@
+@props(['list_item' => null])
+
+<ul class="ti-list">
+    {!! $list_item !!}
+</ul>
---
+++ atoms/ti-list-item.blade.php
@@ -0,0 +1,4 @@
+@props(['list_content' => null])
+<li class="ti-list__content">
+    { !! list_content !!}
+</li>
